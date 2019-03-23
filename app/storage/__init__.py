b49e546886983e49f44877be49a0b5aaa7e3bb7f
Mangust <lakmoes@icloud.com>
2019-03-23 15:11:06 +0200

    Добавил приложение "Хранилище"

--- app/storage/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-storage = Blueprint('storage', __name__)
-
-from . import views
--- instance/config.py
+++
@@ -1,5 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-SECRET_KEY = 'p9Bv<3Eid9%$i01'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
